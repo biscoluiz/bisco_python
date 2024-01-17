cb0c4c86c0494367c1a50d45dd8cb4fdff9f84d9
biscoluiz <66228412+biscoluiz@users.noreply.github.com>
2024-01-16 23:11:15 -0300

    Create random.py

---
+++ 100-days-course/random.py
@@ -0,0 +1,10 @@
+# Write your code below this line 👇
+# Hint: Remember to import the random module first. 🎲
+
+import random
+
+vaar = random.randint(0,1)
+if vaar == 0:
+  print("Tails")
+else:
+  print("Heads")
